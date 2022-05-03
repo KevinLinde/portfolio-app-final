b655a784beae6c0d7feb379535150d3c067bd5f7
KevinLinde <75047430+KevinLinde@users.noreply.github.com>
2022-05-03 03:45:32 -0600

    Add files via upload

---
+++ src/TechnologySummary.js
@@ -0,0 +1,37 @@
+function TechnologySummary() {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h2 className="card-title">Technology Summary</h2>
+                <ul className="list-unstyled">
+                    <li>
+                        <h6 className="fw-bold">Junior Software Developer | Digital Agency 2019-2022</h6>
+                        <p><ul>
+                            <li>HTML, CSS, JS</li>
+                            <li>JSON</li>
+                            <li>Java, SQL</li>
+                        </ul></p>
+                    </li>
+                    <li>
+                        <h6 className="fw-bold">Web Developer | Big Firm 2017-2019</h6>
+                        <p><ul>
+                            <li>HTML, CSS, JS</li>
+                            <li>JSON</li>
+                            <li>Java, SQL</li>
+                        </ul></p>
+                    </li>
+                    <li>
+                        <h6 className="fw-bold">Freelance Web Developer | 2015-2016</h6>
+                        <p><ul>
+                            <li>HTML, CSS, JS</li>
+                            <li>JSON</li>
+                            <li>Java, SQL</li>
+                        </ul></p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default TechnologySummary;
